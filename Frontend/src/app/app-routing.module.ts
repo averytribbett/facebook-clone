01b8219cd4cca6ee745f690e805dbb739c8be847
Avery Tribbett <apatribbett03@gmail.com>
2024-09-24 21:14:30 -0500

    feat(auth): Added basic logged in state. Also added logout button.

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { LoginPageComponent } from './login-page/login-page.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { UserFeedComponent } from './user-feed/user-feed.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'login', component: LoginPageComponent},
   { path: 'profile', component: UserProfileComponent},
   { path: 'feed', component: UserFeedComponent}
 ];
